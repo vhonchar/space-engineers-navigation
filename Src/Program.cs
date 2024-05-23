7c603549709cc546f255f1d4b98992082aae1ca5
Volodymyr Honchar <vhonchar94@gmail.com>
2024-05-22 21:11:59 -0700

    Make some constants configurable through Custom Data of components

--- Src/Program.cs
+++ Src/Program.cs
@@ -10,37 +10,44 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        private const string PanelNameContains = "GPS-Map";
-        private const int broadcastRadius = 700;
+        private string panelNameContains;
+        private const int DefaultDetectionRadius = 700;
         private TimeSpan refreshInterval = TimeSpan.FromSeconds(2);
-
 
         private DateTime lastRefreshTime = DateTime.MinValue;
 
         public Program()
         {
+            InitializeCustomData();
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
         }
 
         public void Main(string argument, UpdateType updateSource)
         {
-            
-            try
+            if ((DateTime.Now - lastRefreshTime) < refreshInterval)
             {
-                if ((DateTime.Now - lastRefreshTime) < refreshInterval)
-                {
-                    return;
-                }
-                Echo($"Running at {lastRefreshTime.TimeOfDay}");
-                var start = DateTime.Now;
-
-                RunScript();
-                lastRefreshTime = DateTime.Now;
-                Echo($"Complete run in {(lastRefreshTime - start).TotalMilliseconds}ms");
-            } catch(Exception ex)
-            {
-                Echo(ex.ToString());
+                return;
             }
+            Echo($"Running at {lastRefreshTime.TimeOfDay}");
+            var start = DateTime.Now;
+
+            RunScript();
+            lastRefreshTime = DateTime.Now;
+            Echo($"Complete run in {(lastRefreshTime - start).TotalMilliseconds}ms");
+        }
+
+        private void InitializeCustomData()
+        {
+            if (!Me.CustomData.Contains("PanelNameContains"))
+            {
+                Me.CustomData = "PanelNameContains=GPS-Map\n" + Me.CustomData;
+            }
+
+            panelNameContains = Me.CustomData
+                .Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault(line => line.StartsWith("PanelNameContains"))
+                ?.Split('=')[1]
+                ?? "GPS-Map";
         }
 
         private void RunScript()
@@ -50,7 +57,7 @@ namespace IngameScript
 
             if (lcdPanels.Count == 0 && cockpits.Count == 0)
             {
-                Echo($"No panel or cockpit with name containing {PanelNameContains} found.");
+                Echo($"No panel or cockpit with name containing {panelNameContains} found.");
                 return;
             }
 
@@ -71,20 +78,22 @@ namespace IngameScript
         {
             List<IMyTextPanel> lcdPanels = new List<IMyTextPanel>();
             GridTerminalSystem.GetBlocksOfType(lcdPanels);
-            return lcdPanels.Where(panel => panel.CustomName.Contains(PanelNameContains)).ToList();
+            return lcdPanels.Where(panel => panel.CustomName.Contains(panelNameContains)).ToList();
         }
 
         private List<IMyCockpit> FindCockpits()
         {
             List<IMyCockpit> cockpits = new List<IMyCockpit>();
-            GridTerminalSystem.GetBlocksOfType(cockpits, cockpit => cockpit.CustomName.Contains(PanelNameContains));
+            GridTerminalSystem.GetBlocksOfType(cockpits, cockpit => cockpit.CustomName.Contains(panelNameContains));
             return cockpits;
         }
 
         private void DrawMapOnLcd(IMyTextPanel lcdPanel)
         {
             Echo($"Panel: '{lcdPanel.CustomName}'");
-            
+
+            int detectionRaius = GetDetectionRadius(lcdPanel);
+
             PrepareTextSurfaceForSprites(lcdPanel);
 
             var frame = lcdPanel.DrawFrame();
@@ -92,15 +101,16 @@ namespace IngameScript
             RandomFlushOfSpriteCache(frame, lcdPanel);
             DrawingUtils.DrawBackground(ref frame, lcdPanel);
             DrawingUtils.DrawAntenna(ref frame, lcdPanel, DrawingUtils.GetCenter(lcdPanel), 1f, 0f, 1.5f);
-            DrawMarkersFromGPS(ref frame, lcdPanel, lcdPanel);
+            DrawMarkersFromGPS(ref frame, lcdPanel, lcdPanel, detectionRaius);
 
             frame.Dispose();
         }
 
-
         private void DrawMapOnCockpit(IMyCockpit cockpit)
         {
             Echo($"Cockpit: '{cockpit.CustomName}'");
+
+            int broadcastRadius = GetDetectionRadius(cockpit);
 
             var screenToDraw = cockpit.GetSurface(0);
             PrepareTextSurfaceForSprites(screenToDraw);
@@ -110,17 +120,26 @@ namespace IngameScript
             RandomFlushOfSpriteCache(frame, screenToDraw);
             DrawingUtils.DrawBackground(ref frame, screenToDraw);
             DrawingUtils.DrawVehicleMark(ref frame, screenToDraw, DrawingUtils.GetCenter(screenToDraw), 1f, 1.5f);
-            DrawMarkersFromGPS(ref frame, cockpit, screenToDraw);
+            DrawMarkersFromGPS(ref frame, cockpit, screenToDraw, broadcastRadius);
 
             frame.Dispose();
         }
 
-        // Game caches list of sprites which are sent to the game client
-        // and doesn't resent a sprite, if its state OR position in anarray of all sprites doesn't change.
-        // And like all systems, game has issues with flushing the cache in multiple cases
-        //
-        // This function will randomly add a blank sprite into that array of sprites, forcing server to resend all sprites after that.
-        // Thus, the script will refresh entire screen once in a while
+        private int GetDetectionRadius(IMyTerminalBlock block)
+        {
+            var configuredDetectionRadius = block.CustomData
+                .Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries)
+                .FirstOrDefault(line => line.StartsWith("DetectionDistance"))
+                ?.Split('=')[1];
+
+            if(string.IsNullOrEmpty(configuredDetectionRadius))
+            {
+                block.CustomData = $"DetectionDistance={DefaultDetectionRadius}\n" + block.CustomData;
+                return DefaultDetectionRadius;
+            }
+            return int.Parse(configuredDetectionRadius);
+        }
+
         private void RandomFlushOfSpriteCache(MySpriteDrawFrame frame, IMyTextSurface drawingSurface)
         {
             if (DateTime.Now.Millisecond % 2 == 0)
@@ -153,10 +172,9 @@ namespace IngameScript
                     textSurface.ScriptBackgroundColor = textSurface.ScriptBackgroundColor.Alpha(1f);
                 }
             }
-
         }
 
-        private void DrawMarkersFromGPS(ref MySpriteDrawFrame frame, IMyTerminalBlock gpsSource, IMyTextSurface drawingSurface)
+        private void DrawMarkersFromGPS(ref MySpriteDrawFrame frame, IMyTerminalBlock gpsSource, IMyTextSurface drawingSurface, int detectionRadius)
         {
             var gpsList = GetGPSList(gpsSource);
             if (gpsList.Count == 0)
@@ -167,7 +185,7 @@ namespace IngameScript
             Echo($"{gpsList.Count} coordinates");
 
             var drawingSurfaceRadius = GetLCDPanelRadius(drawingSurface);
-            var distanceScale = drawingSurfaceRadius / broadcastRadius;
+            var distanceScale = drawingSurfaceRadius / detectionRadius;
 
             foreach (var gps in gpsList)
             {
@@ -176,10 +194,10 @@ namespace IngameScript
                 {
                     var name = parts[1];
                     Vector3D gpsPosition = new Vector3D(double.Parse(parts[2]), double.Parse(parts[3]), double.Parse(parts[4]));
-                    Vector3D gpsPositionInAntennaLocalCoordinates = Vector3D.Transform(gpsPosition, MatrixD.Invert(gpsSource.WorldMatrix)); ;
-                    var proectionTo2D = new Vector2((float)gpsPositionInAntennaLocalCoordinates.X, (float)gpsPositionInAntennaLocalCoordinates.Z);
+                    Vector3D gpsPositionInAntennaLocalCoordinates = Vector3D.Transform(gpsPosition, MatrixD.Invert(gpsSource.WorldMatrix));
+                    var projectionTo2D = new Vector2((float)gpsPositionInAntennaLocalCoordinates.X, (float)gpsPositionInAntennaLocalCoordinates.Z);
 
-                    var markerPosition =  proectionTo2D * distanceScale + DrawingUtils.GetCenter(drawingSurface);
+                    var markerPosition = projectionTo2D * distanceScale + DrawingUtils.GetCenter(drawingSurface);
                     DrawingUtils.DrawMarker(ref frame, markerPosition, name);
                 }
             }
@@ -197,6 +215,5 @@ namespace IngameScript
             var surfaceSize = lcdPanel.SurfaceSize;
             return Math.Min(surfaceSize.X, surfaceSize.Y) / 2f;
         }
-
     }
 }
